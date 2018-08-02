015eef74d456b8be7a39fa435033e44bb321b784
Romonov <Romonov2003@outlook.com>
2018-08-02 14:59:28 +0800

    创建工程！
    Signed-off-by: Romonov <Romonov2003@outlook.com>

---
+++ Mimir/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RUL;
+
+namespace Mimir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Info("Welcome!!");
+            Console.Read();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Mimir
+基于 Authlib-injector 的另一个 Yggdrasil 服务端实现。  
+关于该项目的详细介绍见Wiki。  
+
+## 获取
+您可以通过本项目的 Release 页面下载或者下载源代码并编译来获取最新的 Mimir 。  
+
+## 关于名字
+Mimir 中文音译为“密米尔”，是北欧神话中的密米尔之泉（Well of Mimir），是智慧与知识之泉，坐落在世界之树（Yggdrasil）的第二根树根边，由巨人密米尔（Mimir）所有。（部分摘自中文Wiki“世界之树”页面）  
+
+## Todo
