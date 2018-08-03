6af20d47d67867cbe1a8b5bd062f5dd6461963df
Romonov <Romonov2003@outlook.com>
2018-08-03 13:45:21 +0800

    Http协议

---
+++ Mimir/InitWorker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mimir
+{
+    class InitWorker
+    {
+        public static bool Init()
+        {
+            if (File.Exists(Program.Path + @"/config.ini"))
+            {
+
+            }
+            return true;
+        }
+    }
+}
--- Mimir/Program.cs
+++ Mimir/Program.cs
@@ -1,16 +1,29 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RUL;
+using System.Net;
 
 namespace Mimir
 {
     class Program
     {
+        public static string Path = Directory.GetCurrentDirectory();
+
+        public static HttpListener httpListener = new HttpListener();
+
         static void Main(string[] args)
         {
+            if (!InitWorker.Init())
+            {
+                Logger.Error("Init failed.");
+                Console.Read();
+                return;
+            }
+
             Logger.Info("Welcome!!");
             Console.Read();
         }
