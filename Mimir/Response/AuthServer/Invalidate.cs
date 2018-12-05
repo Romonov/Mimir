89f0767abb430dfc1685a3fccd9b6ba95bfbf7f6
Romonov <Romonov2003@outlook.com>
2018-12-05 12:28:02 +0800

    完成吊销令牌和登出

--- Mimir/Response/AuthServer/Invalidate.cs
+++ Mimir/Response/AuthServer/Invalidate.cs
@@ -1,13 +1,22 @@
-﻿using Newtonsoft.Json;
+﻿using Mimir.Common.SQL;
+using Newtonsoft.Json;
 using System;
+using System.Data;
 
 namespace Mimir.Response.AuthServer
 {
     public class Invalidate
     {
-        public static Tuple<int, string, string> OnPost(string PostData)
+        public static Tuple<int, string, string> OnPost(string postData)
         {
-            JsonConvert.DeserializeObject<Request>(PostData);
+            Request request = JsonConvert.DeserializeObject<Request>(postData);
+
+            DataSet dataSetToken = SqlProxy.Query($"select * from `tokens` where `AccessToken` = '{SqlSecurity.Parse(request.accessToken)}';");
+
+            if (dataSetToken?.Tables[0]?.Rows?.Count >= 1)
+            {
+                SqlProxy.Excuter($"update `tokens` set `Status` = 0 where `AccessToken` = '{SqlSecurity.Parse(request.accessToken)}';");
+            }
 
             return new Tuple<int, string, string>(204, "text/plain", "");
         }
--- Mimir/Response/AuthServer/Signout.cs
+++ Mimir/Response/AuthServer/Signout.cs
@@ -15,24 +15,20 @@ namespace Mimir.Response.AuthServer
             Request request = JsonConvert.DeserializeObject<Request>(PostData);
 
             // Login
-            DataSet dataSetUser = SqlProxy.Query($"SELECT * FROM users");
+            DataSet dataSetUser = SqlProxy.Query($"select * from `users` where `Username` = '{SqlSecurity.Parse(request.username)}' and `Password` = '{HashWorker.MD5(request.password)}';");
+            DataRow dataRowUser;
 
-            DataRow userRow = null;
-            
-            foreach (DataRow dataRow in dataSetUser.Tables[0].Rows)
+            if (dataSetUser?.Tables[0]?.Rows.Count >= 1)
             {
-                if (dataRow["Email"].ToString() == request.username && dataRow["Password"].ToString() == HashWorker.MD5(request.password))
-                {
-                    userRow = dataRow;
-                }
-                else
-                {
-                    return InvalidPassword.GetResponse();
-                }
+                dataRowUser = dataSetUser.Tables[0].Rows[0];
+            }
+            else
+            {
+                return InvalidPassword.GetResponse();
             }
 
             // Signout
-            SqlProxy.Excuter($"UPDATE `tokens` SET `Status` = 2 WHERE `BindUser` = {userRow["Username"].ToString()}");
+            SqlProxy.Excuter($"update `tokens` set `Status` = 0 where `BindUser` = {dataRowUser["Username"].ToString()}");
 
             return new Tuple<int, string, string>(204, "text/plain", "");
         }
